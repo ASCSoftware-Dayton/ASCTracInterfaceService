3c113eefb516f89a0693851f608c55dd6b09a6da
ascuser <mhill@ascsoftware.com>
2022-05-31 13:38:37 -0400

    Add Cust Order Ship Import

---
+++ ASCTracInterfaceService/Controllers/Import/CustOrderShipImportController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASCTracInterfaceService.Controllers.Import
+{
+    public class CustOrderShipImportController : ApiController
+    {
+        [System.Web.Http.HttpPut]
+        public HttpResponseMessage PostCustOrderShip(string aOrderNumber)
+        {
+            HttpStatusCode statusCode = HttpStatusCode.Accepted;
+            string errMsg = string.Empty;
+            try
+            {
+                statusCode = ASCTracInterfaceDll.Imports.ImportCustOrder.doImportCustOrderConfirmShip(aOrderNumber, ref errMsg);
+            }
+            catch (Exception ex)
+            {
+                statusCode = HttpStatusCode.BadRequest;
+                errMsg = ex.Message;
+            }
+            var retval = new HttpResponseMessage(statusCode);
+            retval.Content = new StringContent(errMsg);
+            //var retval = new Models.ModelReturnType(errMsg);
+            return (retval);
+        }
+    }
+}
