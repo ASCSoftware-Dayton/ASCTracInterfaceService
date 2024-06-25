687373a5b0426f5769e0c8ddced7e8e12f00d407
ascuser <mhill@ascsoftware.com>
2024-06-25 10:05:50 -0400

    14.1 Beta

--- ASCTracInterfaceService/Properties/AssemblyInfo.cs
+++ ASCTracInterfaceService/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ASC Software")]
 [assembly: AssemblyProduct("ASCTracInterfaceService")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2022-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("11.0.0.0")]
-[assembly: AssemblyFileVersion("11.0.0.0")]
+[assembly: AssemblyVersion("14.1.0.0")]
+[assembly: AssemblyFileVersion("14.1.0.0")]
